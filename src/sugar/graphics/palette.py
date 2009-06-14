4bb221349c24515303fa14f9a580f955825b17d4
Benjamin Berg <benjamin@sipsolutions.net>
2009-06-14 14:08:52 +0200

    Emit a row_changed signal to redraw the CellRendererIcon on prelight.
    This fixes a problem where the wrong part of the treeview was redrawn.

--- src/sugar/graphics/palette.py
+++ src/sugar/graphics/palette.py
@@ -1220,12 +1220,9 @@ class CellRendererInvoker(Invoker):
             self.notify_mouse_leave()
 
     def _redraw_path(self, path):
-        for column in self._tree_view.get_columns():
-            if self._cell_renderer in column.get_cell_renderers():
-                break
-        area = self._tree_view.get_background_area(path, column)
-        x, y = self._tree_view.convert_tree_to_widget_coords(area.x, area.y)
-        self._tree_view.queue_draw_area(x, y, area.width, area.height)
+        model = self._tree_view.get_model()
+        iter = model.get_iter(path)
+        model.row_changed(path, iter)
 
     def __leave_notify_event_cb(self, widget, event):
         self.notify_mouse_leave()
