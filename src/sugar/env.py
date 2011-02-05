sugarlabs/sugar-toolkit-gtk3

commit 814ab2ddf9337c36244929840c296b26430eb045

PEP8 cleanup: don't use has_key()
has_key() has been deprecated for quite some time now.

Acked-by: Simon Schampijer <simon@laptop.org>
CC: Aleksey Lim <alsroot@member.fsf.org>

src/sugar/env.py
@@ -24,18 +24,11 @@ import os
 
 
 def is_emulator():
-    if os.environ.has_key('SUGAR_EMULATOR'):
-        if os.environ['SUGAR_EMULATOR'] == 'yes':
-            return True
-    return False
+    return os.environ.get('SUGAR_EMULATOR', 'yes') == 'yes'
 
 
 def get_profile_path(path=None):
-    if os.environ.has_key('SUGAR_PROFILE'):
-        profile_id = os.environ['SUGAR_PROFILE']
-    else:
-        profile_id = 'default'
-
+    profile_id = os.environ.get('SUGAR_PROFILE', 'default')
     base = os.path.join(os.path.expanduser('~/.sugar'), profile_id)
     if not os.path.isdir(base):
         try:

src/sugar/activity/activity.py
@@ -256,7 +256,7 @@ class Activity(Window, gtk.Container):
         """
         Window.__init__(self)
 
-        if os.environ.has_key('SUGAR_ACTIVITY_ROOT'):
+        if 'SUGAR_ACTIVITY_ROOT' in os.environ:
             # If this activity runs inside Sugar, we want it to take all the
             # screen. Would be better if it was the shell to do this, but we
             # haven't found yet a good way to do it there. See #1263.
@@ -307,7 +307,7 @@ class Activity(Window, gtk.Container):
             self._jobject = datastore.get(handle.object_id)
             self.set_title(self._jobject.metadata['title'])
 
-            if self._jobject.metadata.has_key('share-scope'):
+            if 'share-scope' in self._jobject.metadata:
                 share_scope = self._jobject.metadata['share-scope']
 
         self.shared_activity = None
@@ -509,8 +509,7 @@ class Activity(Window, gtk.Container):
         which isn't specific to a journal item here. If (meta-)data is in
         anyway specific to a journal entry, it MUST be stored in the DataStore.
         """
-        if os.environ.has_key('SUGAR_ACTIVITY_ROOT') and \
-           os.environ['SUGAR_ACTIVITY_ROOT']:
+        if os.environ.get('SUGAR_ACTIVITY_ROOT'):
             return os.environ['SUGAR_ACTIVITY_ROOT']
         else:
             return '/'
@@ -974,8 +973,7 @@ def get_bundle_path():
 
 def get_activity_root():
     """Returns a path for saving Activity specific preferences, etc."""
-    if os.environ.has_key('SUGAR_ACTIVITY_ROOT') and \
-            os.environ['SUGAR_ACTIVITY_ROOT']:
+    if os.environ.get('SUGAR_ACTIVITY_ROOT'):
         return os.environ['SUGAR_ACTIVITY_ROOT']
     else:
         raise RuntimeError("No SUGAR_ACTIVITY_ROOT set.")

src/sugar/activity/namingalert.py
@@ -271,8 +271,7 @@ class NamingAlert(gtk.Window):
             activity_bundle = ActivityBundle(self._bundle_path)
             file_name = activity_bundle.get_icon()
         entry_icon = CanvasIcon(file_name=file_name)
-        if self._activity.metadata.has_key('icon-color') and \
-                self._activity.metadata['icon-color']:
+        if self._activity.metadata.get('icon-color'):
             entry_icon.props.xo_color = XoColor( \
                 self._activity.metadata['icon-color'])
         return entry_icon

src/sugar/graphics/palettegroup.py
@@ -26,7 +26,7 @@ _groups = {}
 
 
 def get_group(group_id):
-    if _groups.has_key(group_id):
+    if group_id in _groups:
         group = _groups[group_id]
     else:
         group = Group()

src/sugar/graphics/style.py
@@ -35,12 +35,11 @@ _TAB_CURVATURE = 1
 
 
 def _compute_zoom_factor():
-    if os.environ.has_key('SUGAR_SCALING'):
-        try:
-            scaling = int(os.environ['SUGAR_SCALING'])
-            return scaling / 100.0
-        except ValueError:
-            logging.error('Invalid SUGAR_SCALING.')
+    try:
+        scaling = int(os.environ.get('SUGAR_SCALING', '100'))
+        return scaling / 100.0
+    except ValueError:
+        logging.error('Invalid SUGAR_SCALING.')
 
     return 1.0
 

src/sugar/network.py
@@ -242,7 +242,7 @@ class GlibURLDownloader(gobject.GObject):
         self.cleanup(remove=True)
 
     def _get_filename_from_headers(self, headers):
-        if not headers.has_key("Content-Disposition"):
+        if 'Content-Disposition' not in headers:
             return None
 
         ftag = "filename="

commit 924e2e62bf9e2dbc7a15c412def076a0194370a3

fix sugar-emulator detection logic broken by 814ab2d (has_key() cleanup)
This broke Shutdown and Restart (buddy menu).

Signed-off-by: Sascha Silbe <silbe@activitycentral.com>

src/sugar/env.py
@@ -24,7 +24,7 @@ import os
 
 
 def is_emulator():
-    return os.environ.get('SUGAR_EMULATOR', 'yes') == 'yes'
+    return os.environ.get('SUGAR_EMULATOR', 'no') == 'yes'
 
 
 def get_profile_path(path=None):