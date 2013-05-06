3a8760c9e4dff2ebb91f7f5772f6788cce0267dc
William Orr <will@worrbase.com>
2013-05-05 23:14:57 -0400

    Fixed crash in journal when mousing over activity icons

--- src/sugar3/graphics/palettewindow.py
+++ src/sugar3/graphics/palettewindow.py
@@ -994,9 +994,9 @@ class Invoker(GObject.GObject):
             # menu item. We need to postpone destruction of the palette until
             # after all signals have propagated from the menu item to the
             # palette owner.
-            GObject.idle_add(lambda old_palette=self._palette:
-                             old_palette.destroy(),
-                             priority=GObject.PRIORITY_LOW)
+            GLib.idle_add(lambda old_palette=self._palette:
+                          old_palette.destroy(),
+                          priority=GObject.PRIORITY_LOW)
 
         self._palette = palette
 
