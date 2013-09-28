9c7ca8457c17ef2bd189afaaa592b5ba9f7dd896
Daniel Narvaez <dwnarvaez@gmail.com>
2013-09-28 11:33:40 +0200

    Use python2 shebang
    Some distributions have switched to python3 as default and our code
    breaks because it's python2 specific. Follow the PEP 394
    recommendation.

--- tests/data/sample.activity/setup.py
+++ tests/data/sample.activity/setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 from sugar3.activity import bundlebuilder
 
--- tests/test_mime.py
+++ tests/test_mime.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 # Copyright (C) 2006, Red Hat, Inc.
 # Copyright (C) 2007, One Laptop Per Child
