c65dabb90a681cd8885694b8477d4b9f659229e4
Prasoon Shukla <prasoon92.iitr@gmail.com>
2014-04-02 19:32:34 +0530

    Fix for ticket 4686.
    Added two Gtk.Alignment widgets, one each for the primary as well
    as the secondary labels in the palette.

--- src/sugar3/graphics/palette.py
+++ src/sugar3/graphics/palette.py
@@ -22,6 +22,8 @@
 """
 STABLE.
 """
+import textwrap
+
 from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GObject
@@ -133,23 +135,30 @@ class Palette(PaletteWindow):
 
         self._label = Gtk.AccelLabel(label='')
         self._label.set_alignment(0, 0.5)
+        self._primary_label_alignment = Gtk.Alignment(xalign=0, yalign=0.5,
+                                                      xscale=1, yscale=1)
+        self._primary_label_alignment.add(self._label)
+        self._primary_label_alignment.show()
 
         if text_maxlen > 0:
             self._label.set_max_width_chars(text_maxlen)
             self._label.set_ellipsize(Pango.EllipsizeMode.MIDDLE)
-        labels_box.pack_start(self._label, True, True, 0)
+        labels_box.pack_start(self._primary_label_alignment, True, True, 0)
         self._primary_event_box.connect('button-release-event',
                                         self.__button_release_event_cb)
         self._primary_event_box.set_events(Gdk.EventMask.BUTTON_RELEASE_MASK)
 
         self._secondary_label = Gtk.Label()
         self._secondary_label.set_alignment(0, 0.5)
+        self._secondary_label_alignment = Gtk.Alignment(xalign=0, yalign=0.5,
+                                                        xscale=1, yscale=1)
+        self._secondary_label_alignment.add(self._secondary_label)
 
         if text_maxlen > 0:
             self._secondary_label.set_max_width_chars(text_maxlen)
             self._secondary_label.set_ellipsize(Pango.EllipsizeMode.END)
 
-        labels_box.pack_start(self._secondary_label, True, True, 0)
+        labels_box.pack_start(self._secondary_label_alignment, True, True, 0)
 
         self._secondary_box = Gtk.VBox()
 
@@ -266,15 +275,45 @@ class Palette(PaletteWindow):
             self.props.invoker.primary_text_clicked()
 
     def set_secondary_text(self, label):
-        if label is not None:
-            label = label.split('\n', 1)[0]
-        self._secondary_text = label
-
         if label is None:
+            self._secondary_label_alignment.hide()
             self._secondary_label.hide()
         else:
+            NO_OF_LINES = 3
+            ELLIPSIS_LENGTH = 6
+
+            label = label.replace('\n', ' ')
+
+            self._secondary_label.modify_fg(Gtk.StateType.INSENSITIVE,
+                                            Gdk.color_parse('white'))
+            self._secondary_label.set_justify(Gtk.Justification.FILL)
+            self._secondary_label.set_alignment(0, 0)
+
+            # for Gtk >= 3.10
+            if hasattr(self._secondary_label, 'set_lines'):
+                self._secondary_label.set_max_width_chars(
+                    style.MENU_WIDTH_CHARS)
+                self._secondary_label.set_line_wrap(True)
+                self._secondary_label.set_ellipsize(
+                    Pango.EllipsizeMode.END)
+                self._secondary_label.set_lines(3)
+            else:
+                # FIXME: fallback for Gtk < 3.10
+                body_width = NO_OF_LINES * style.MENU_WIDTH_CHARS
+                body_width -= ELLIPSIS_LENGTH
+                if len(label) > body_width:
+                    label = ' '.join(label[:body_width].split()[:-1]) + '...'
+                label = textwrap.fill(label, width=style.MENU_WIDTH_CHARS)
+
+            if len(label) > style.MENU_WIDTH_CHARS:
+                pad = style.DEFAULT_SPACING
+                self._primary_label_alignment.set_padding(pad, 0, 0, 0)
+                self._secondary_label_alignment.set_padding(0, pad, 0, pad)
+
+            self._secondary_text = label
             self._secondary_label.set_text(label)
             self._secondary_label.show()
+            self._secondary_label_alignment.show()
 
     def get_secondary_text(self):
         return self._secondary_text
