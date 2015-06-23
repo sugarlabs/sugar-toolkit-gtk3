sugarlabs/sugar-toolkit-gtk3

commit 034706a482deef3db5e8a93b232882af3224016c

Secondary text on palettes should respect the max width - Fixes #4862
When a activity have a title very long without spaces,
the text can't be wraped with the default wrap mode (WORD)
Use WRAP_CHAR to solve this issue.

src/sugar3/graphics/palette.py
@@ -286,6 +286,7 @@ class Palette(PaletteWindow):
                 self._secondary_label.set_line_wrap(True)
                 self._secondary_label.set_ellipsize(
                     style.ELLIPSIZE_MODE_DEFAULT)
+                self._secondary_label.set_wrap_mode(Gtk.WrapMode.WORD_CHAR)
                 self._secondary_label.set_lines(NO_OF_LINES)
                 self._secondary_label.set_justify(Gtk.Justification.FILL)
             else:

commit 158de7bdd7875362721790defe75a493becabad2

Fix set_wrap_mode does not exsist error
Commit 034706a introduced an error on Fedora rawhide (23):

    Traceback (most recent call last):
      File "/usr/lib64/python2.7/site-packages/gi/_propertyhelper.py", line 427, in obj_set_property
        prop.fset(self, value)
      File "/home/sam/sb21/build/out/install/lib/python2.7/site-packages/sugar3/graphics/palette.py", line 289, in set_secondary_text
        self._secondary_label.set_wrap_mode(Gtk.WrapMode.WORD_CHAR)
    AttributeError: 'Label' object has no attribute 'set_wrap_mode'

src/sugar3/graphics/palette.py
@@ -286,7 +286,7 @@ class Palette(PaletteWindow):
                 self._secondary_label.set_line_wrap(True)
                 self._secondary_label.set_ellipsize(
                     style.ELLIPSIZE_MODE_DEFAULT)
-                self._secondary_label.set_wrap_mode(Gtk.WrapMode.WORD_CHAR)
+                self._secondary_label.props.wrap_mode = Gtk.WrapMode.WORD_CHAR
                 self._secondary_label.set_lines(NO_OF_LINES)
                 self._secondary_label.set_justify(Gtk.Justification.FILL)
             else: