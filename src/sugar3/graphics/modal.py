f4f8a078a8853799b5b1b99ba4896a705130de08
Sam Parkinson <sam.parkinson3@gmail.com>
2015-07-07 13:28:42 +1000

    Add SelectorModal and the backing SelectorModel

--- src/sugar3/graphics/modal.py
+++ src/sugar3/graphics/modal.py
@@ -22,8 +22,10 @@ from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GObject
 
+from sugar3 import profile
 from sugar3.graphics import style
-from sugar3.graphics.icon import Icon
+from sugar3.graphics.icon import CellRendererIcon
+from sugar3.graphics.iconentry import IconEntry, ICON_ENTRY_PRIMARY
 from sugar3.graphics.toolbutton import ToolButton
 from jarabe.model import shell
 
@@ -132,3 +134,113 @@ class Modal(Gtk.Window):
         self.cancel_clicked_signal.emit()
         self.destroy()
 
+
+class SelectorModal(Modal):
+
+    item_selected_signal = GObject.Signal('item-selected',
+                                          arg_types=([object]))
+
+    def __init__(self):
+        '''
+        A Modal popup that has a list of items.
+        The user selects one item and that emits the 'item-selected' signal.
+        If the user exits the modal, the 'cancel-clicked' signal is emitted'
+        '''
+        Modal.__init__(self)
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        box.modify_bg(Gtk.StateType.NORMAL, style.COLOR_WHITE.get_gdk_color())
+        self.set_content(box)
+        box.show()
+
+        self._search = IconEntry()
+        self._search.placeholder_text = _('Search')
+        self._search.add_clear_button()
+        self._search.set_icon_from_name(ICON_ENTRY_PRIMARY, 'system-search')
+        box.pack_start(self._search, False, False, style.DEFAULT_SPACING)
+        self._search.show()
+        self._search.grab_focus()
+
+        self.model = SelectorModel()
+        self._filter_model = self.model.filter_new()
+        self._filter_model.set_visible_func(self.__model_filter_cb)
+        self._search.connect('changed',
+                             lambda *args: self._filter_model.refilter())
+        self._search.connect('activate', self.__search_activate_cb)
+
+        self._tree_view = Gtk.TreeView(self._filter_model)
+        self._tree_view.props.headers_visible = False
+        self._tree_view.props.activate_on_single_click = True
+        box.pack_start(self._tree_view, True, True, 0)
+        self._tree_view.connect('row-activated', self.__tree_view_activate_cb)
+        self._tree_view.show()
+
+        cell_icon = _CellRendererIcon()
+        column = Gtk.TreeViewColumn()
+        column.props.sizing = Gtk.TreeViewColumnSizing.FIXED
+        column.props.fixed_width = cell_icon.props.width
+        column.pack_start(cell_icon, True)
+        column.add_attribute(cell_icon, 'icon-name',
+                             SelectorModel.COLUMN_ICON_NAME)
+        column.add_attribute(cell_icon, 'file-name',
+                             SelectorModel.COLUMN_ICON_FILE)
+        column.add_attribute(cell_icon, 'xo-color',
+                             SelectorModel.COLUMN_ICON_COLOR)
+        self._tree_view.append_column(column)
+
+        cell_text = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn()
+        column.pack_start(cell_text, True)
+        column.add_attribute(cell_text, 'text', SelectorModel.COLUMN_TEXT)
+        self._tree_view.append_column(column)
+
+    def __model_filter_cb(self, model, row, data):
+        query = self._search.get_text().lower()
+        text = self.model.get_value(row, SelectorModel.COLUMN_TEXT).lower()
+        return query in text
+
+    def __search_activate_cb(self, entry):
+        row = self._filter_model.get_iter(Gtk.TreePath.new_first())
+        if row is not None:
+            data = self._filter_model.get_value(row, SelectorModel.COLUMN_DATA)
+            self.item_selected_signal.emit(data)
+            self.destroy()
+
+    def __tree_view_activate_cb(self, tree_view, path, column):
+        row = self._filter_model.get_iter(path)
+        data = self._filter_model.get_value(row, SelectorModel.COLUMN_DATA)
+        self.item_selected_signal.emit(data)
+        self.destroy()
+
+
+class SelectorModel(Gtk.ListStore):
+
+    COLUMN_ICON_NAME = 0
+    COLUMN_ICON_FILE = 1
+    COLUMN_ICON_COLOR = 2
+    COLUMN_TEXT = 3
+    COLUMN_DATA = 4
+
+    COLUMN_TYPES = [str, str, object, str, object]
+
+    def __init__(self):
+        Gtk.ListStore.__init__(self, *self.COLUMN_TYPES)
+
+    def add_item(self, text, icon_name=None, icon_file=None,
+                 icon_color=None, data=None):
+        if icon_name is None and icon_file is None:
+            raise ValueError('Either icon_name or icon_file is required')
+        if icon_color is None:
+            icon_color = profile.get_color()
+        if data is None:
+            data = text
+        self.append([icon_name, icon_file, icon_color, text, data])
+
+
+class _CellRendererIcon(CellRendererIcon):
+    def __init__(self):
+        CellRendererIcon.__init__(self)
+
+        self.props.width = style.GRID_CELL_SIZE
+        self.props.height = style.GRID_CELL_SIZE
+        self.props.size = style.STANDARD_ICON_SIZE
+        self.props.mode = Gtk.CellRendererMode.ACTIVATABLE
