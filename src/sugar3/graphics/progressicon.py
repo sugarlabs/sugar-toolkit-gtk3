7afb4972a231df29ce98786942e509cdfc53a9ef
Hrishi1999 <hrishipatel_5@outlook.com>
2015-12-26 22:09:54 +0530

    Update progressicon.py

--- src/sugar3/graphics/progressicon.py
+++ src/sugar3/graphics/progressicon.py
@@ -14,10 +14,25 @@
 # License along with this library; if not, write to the
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
+'''
+The progressicon module provide a progress icon; a widget like progress
+bar which shows progress of a task
+'''
+
+'''
+The above three lines are for importing modules. The first one imports GTK form gi.repository, second one imports get_surface from 
+sugar3.graphics.icon and third one imports style namespace from sugar3.graphics.
+'''
+
 
 from gi.repository import Gtk
 from sugar3.graphics.icon import get_surface
 from sugar3.graphics import style
+
+
+'''
+Here is the class 'ProgressIcon'
+'''
 
 
 class ProgressIcon(Gtk.DrawingArea):
@@ -26,7 +41,6 @@ class ProgressIcon(Gtk.DrawingArea):
     This class is compatible with the sugar3.graphics.icon.Icon class.
 
     Call update(progress) with the new progress to update the icon.
-
     The direction defaults to 'vertical', in which case the icon is
     filled from bottom to top.  If direction is set to 'horizontal',
     it will be filled from right to left or from left to right,
@@ -37,49 +51,58 @@ class ProgressIcon(Gtk.DrawingArea):
                  direction='vertical'):
         Gtk.DrawingArea.__init__(self)
 
-        self._icon_name = icon_name
-        self._direction = direction
-        self._progress = 0
+        self._icon_name = icon_name '''Icon Name '''
+        self._direction = direction ''' Direction'''
+        self._progress = 0 '''Initial Value of progress is zero'''
 
         self._stroke = get_surface(
             icon_name=icon_name, width=pixel_size, height=pixel_size,
             stroke_color=stroke_color,
-            fill_color=style.COLOR_TRANSPARENT.get_svg())
-
+            fill_color=style.COLOR_TRANSPARENT.get_svg())  
+       
+        ''' icon_name=icon_name means the icon_name after '=' is from get_surface, width is parsed from pixel_size of get_surface
+            similarly, height is also parsed from pixel_size of get_surface (like XXXpx), the fill_color means fill color of progress
+            icon and its color is imported from namespace style. Here, it's color is transparent. 
+        '''
+       
         self._fill = get_surface(
             icon_name=icon_name, width=pixel_size, height=pixel_size,
             stroke_color=style.COLOR_TRANSPARENT.get_svg(),
             fill_color=fill_color)
-
+        
+         ''' icon_name=icon_name means the icon_name after '=' is from get_surface, width is parsed from pixel_size of get_surface
+            similarly, height is also parsed from pixel_size of get_surface (like XXXpx). The stroke color changes the color of stroke
+            to transparent(here).
+         '''
         self.connect("draw", self.__draw_cb)
 
     def __draw_cb(self, widget, cr):
         allocation = widget.get_allocation()
 
         # Center the graphic in the allocated space.
-        margin_x = (allocation.width - self._stroke.get_width()) / 2
-        margin_y = (allocation.height - self._stroke.get_height()) / 2
+        margin_x = (allocation.width - self._stroke.get_width()) / 2 '''Margin x : It's value is width from allocation - stroke's width '''
+        margin_y = (allocation.height - self._stroke.get_height()) / 2 '''Margin y : It's value is height from allocation - stroke's height '''
         cr.translate(margin_x, margin_y)
 
         # Paint the fill, clipping it by the progress.
-        x_, y_ = 0, 0
-        width, height = self._stroke.get_width(), self._stroke.get_height()
-        if self._direction == 'vertical':  # vertical direction, bottom to top
+        x_, y_ = 0, 0 '''The value of margin x_ and y_ is 0,0 (0i,0j) '''
+        width, height = self._stroke.get_width(), self._stroke.get_height() '''Width = self._stroke.get_width() & height = self._stroke.get_height()'''
+        if self._direction == 'vertical':  '''if _direction is vertical then''' #vertical direction, bottom to top 
             y_ = self._stroke.get_height()
-            height *= self._progress * -1
-        else:
+            height *= self._progress * -1 '''Vale of _progress (mentioned above) is multiplied with -1 ''' 
+        else: '''if self._direction is not vertical'''
             rtl_direction = \
-                Gtk.Widget.get_default_direction() == Gtk.TextDirection.RTL
-            if rtl_direction:  # horizontal direction, right to left
-                x_ = self._stroke.get_width()
+                Gtk.Widget.get_default_direction() == Gtk.TextDirection.RTL '''default direction'''
+            if rtl_direction: '''IF rtl_direction ----> right to left '''  # horizontal direction, right to left
+                x_ = self._stroke.get_width() '''value of x_ gets set as self._stroke.get_width() '''
                 width *= self._progress * -1
-            else:  # horizontal direction, left to right
+            else: '''IF rtl_direction ----> left to right  '''  # horizontal direction, left to right
                 width *= self._progress
 
-        cr.rectangle(x_, y_, width, height)
-        cr.clip()
+        cr.rectangle(x_, y_, width, height) '''x_ = width and y_ = width '''
+        cr.clip() '''Clip'''
         cr.set_source_surface(self._fill, 0, 0)
-        cr.paint()
+        cr.paint() '''Applied'''
 
         # Paint the stroke over the fill.
         cr.reset_clip()
@@ -87,13 +110,13 @@ class ProgressIcon(Gtk.DrawingArea):
         cr.paint()
 
     def do_get_preferred_width(self):
-        width = self._stroke.get_width()
-        return (width, width)
+        width = self._stroke.get_width() '''width will be set as self._stroke.get_width()'''
+        return (width, width) '''Returns method'''
 
     def do_get_preferred_height(self):
-        height = self._stroke.get_height()
-        return (height, height)
+        height = self._stroke.get_height() '''height will be set as self._stroke.get_height()'''
+        return (height, height) '''Returns method'''
 
     def update(self, progress):
-        self._progress = progress
-        self.queue_draw()
+        self._progress = progress '''_progress's value is equal to progress's value'''
+        self.queue_draw() '''Draws'''
