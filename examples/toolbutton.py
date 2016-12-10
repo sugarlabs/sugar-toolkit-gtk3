e0b5ef5a6555ce95e3ad0ec9e090e30360ef18f7
Hrishi <hrishipatel99@gmail.com>
2016-12-10 14:16:33 +0530

    Update toolbutton.py

--- examples/toolbutton.py
+++ examples/toolbutton.py
@@ -1,6 +1,6 @@
 from gi.repository import Gtk
 
-from sugar3.graphics.icon import Icon
+from sugar3.graphics.toolbarbox import ToolbarBox
 from sugar3.graphics.toolbutton import ToolButton
 
 import common
@@ -9,10 +9,21 @@ import common
 test = common.Test()
 test.show()
 
-tb1 = ToolButton()
-tb1.show()
+vbox = Gtk.VBox()
+test.pack_start(vbox, True, True, 0)
+vbox.show()
 
-tb2 = ToolButton(icon_name='document-send', tooltip='document-send')
-tb2.show()
+toolbar_box = ToolbarBox()
+vbox.pack_start(toolbar_box, False, False, 0)
+toolbar_box.show()
 
-Gtk.main()
+separator = Gtk.SeparatorToolItem()
+toolbar_box.toolbar.insert(separator, -1)
+separator.show()
+
+Tool_Button = ToolButton(icon_name='view-radial')
+toolbar_box.toolbar.insert(Tool_Button, -1)
+Tool_Button.show()
+
+if __name__ == '__main__':
+    common.main(test)
