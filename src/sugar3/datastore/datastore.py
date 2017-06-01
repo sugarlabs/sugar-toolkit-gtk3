286387b22e70ffe15f51e782ebf48a68ac3646f1
James Cameron <quozl@laptop.org>
2017-06-01 12:57:34 +1000

    Save As - return copied datastore object
    When a datastore object is copied, return the new object to the caller.

--- src/sugar3/datastore/datastore.py
+++ src/sugar3/datastore/datastore.py
@@ -470,6 +470,9 @@ def copy(ds_object, mount_point):
     ds_object -- DSObject to copy
     mount_point -- mount point of the new datastore entry
 
+    Returns:
+    new_ds_object -- DSObject copied
+
     """
     new_ds_object = ds_object.copy()
     new_ds_object.metadata['mountpoint'] = mount_point
@@ -489,6 +492,8 @@ def copy(ds_object, mount_point):
     new_ds_object.file_path = ds_object.file_path
 
     write(new_ds_object)
+
+    return new_ds_object
 
 
 def get_unique_values(key):
