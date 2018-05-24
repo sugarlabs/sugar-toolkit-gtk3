sugarlabs/sugar-toolkit-gtk3

commit f7966382b88cebad9d20fb8537d3247886a8d1d3

Read icon attach points when is not possible get the info from gtk
Gtk has deprecated gtk-icon-get-attach-points on version 3.14
We need that information to place pur badges,
this fix read the information from the .icon file

[1] https://developer.gnome.org/gtk3/stable/GtkIconTheme.html#gtk-icon-info-get-attach-points

src/sugar3/graphics/icon.py
@@ -24,6 +24,8 @@ STABLE.
 import re
 import math
 import logging
+import os
+from ConfigParser import ConfigParser
 
 from gi.repository import GObject
 from gi.repository import Gtk
@@ -124,12 +126,27 @@ class _IconBuffer(object):
 
     def _get_attach_points(self, info, size_request):
         has_attach_points_, attach_points = info.get_attach_points()
-
+        attach_x = attach_y = 0
         if attach_points:
+            # this works only for Gtk < 3.14
+            # https://developer.gnome.org/gtk3/stable/GtkIconTheme.html
+            # #gtk-icon-info-get-attach-points
             attach_x = float(attach_points[0].x) / size_request
             attach_y = float(attach_points[0].y) / size_request
-        else:
-            attach_x = attach_y = 0
+        elif info.get_filename():
+            # try read from the .icon file
+            icon_filename = info.get_filename().replace('.svg', '.icon')
+            if os.path.exists(icon_filename):
+                try:
+                    with open(icon_filename) as config_file:
+                        cp = ConfigParser()
+                        cp.readfp(config_file)
+                        attach_points_str = cp.get('Icon Data', 'AttachPoints')
+                        attach_points = attach_points_str.split(',')
+                        attach_x = float(attach_points[0].strip()) / 1000
+                        attach_y = float(attach_points[1].strip()) / 1000
+                except Exception as e:
+                    logging.exception('Exception reading icon info: %s', e)
 
         return attach_x, attach_y
 

commit 676f49ebc4a637a09ca1dbe4c5512184a1021928

Fix traceback when an icon name matches a GNOME theme icon
When an icon file name matches a GNOME theme icon;

Traceback (most recent call last):
  File "/usr/lib/python2.7/dist-packages/sugar3/graphics/icon.py", line 214, in _get_attach_points
    cp.readfp(config_file)
  File "/usr/lib/python2.7/ConfigParser.py", line 324, in readfp
    self._read(fp, filename)
  File "/usr/lib/python2.7/ConfigParser.py", line 512, in _read
    raise MissingSectionHeaderError(fpname, lineno, line)
MissingSectionHeaderError: File contains no section headers.

Caused by reading a .png file as if it were config file, in turn
because the icon was expected by the toolkit to be .svg file type.

Regression introduced by f796638.

Fixes https://github.com/sugarlabs/activity-abacus/issues/16

src/sugar3/graphics/icon.py
@@ -205,7 +205,9 @@ class _IconBuffer(object):
         elif info.get_filename():
             # try read from the .icon file
             icon_filename = info.get_filename().replace('.svg', '.icon')
-            if os.path.exists(icon_filename):
+            if icon_filename != info.get_filename() and \
+                os.path.exists(icon_filename):
+
                 try:
                     with open(icon_filename) as config_file:
                         cp = ConfigParser()