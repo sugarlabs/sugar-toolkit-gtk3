sugarlabs/sugar-toolkit-gtk3

commit cb0d6e2ef8859ecc69d902ce1931e7910d6264c9

Port from GObject to GLib

src/sugar3/activity/activityfactory.py
@@ -26,6 +26,7 @@ import logging
 
 import dbus
 from gi.repository import GObject
+from gi.repository import GLib
 
 from sugar3.activity.activityhandle import ActivityHandle
 from sugar3 import util
@@ -233,10 +234,10 @@ class ActivityCreationHandler(GObject.GObject):
                                  stdout=log_file.fileno(),
                                  stderr=log_file.fileno())
 
-        GObject.child_watch_add(child.pid,
-                                _child_watch_cb,
-                                (log_file,
-                                 self._handle.activity_id))
+        GLib.child_watch_add(child.pid,
+                             _child_watch_cb,
+                             (log_file,
+                             self._handle.activity_id))
 
     def _no_reply_handler(self, *args):
         pass

src/sugar3/graphics/tray.py
@@ -36,9 +36,9 @@ _NEXT_PAGE = 1
 class _TrayViewport(Gtk.Viewport):
 
     __gproperties__ = {
-        'scrollable': (bool, None, None, False, GObject.PARAM_READABLE),
-        'can-scroll-prev': (bool, None, None, False, GObject.PARAM_READABLE),
-        'can-scroll-next': (bool, None, None, False, GObject.PARAM_READABLE),
+        'scrollable': (bool, None, None, False, GObject.ParamFlags.READABLE),
+        'can-scroll-prev': (bool, None, None, False, GObject.ParamFlags.READABLE),
+        'can-scroll-next': (bool, None, None, False, GObject.ParamFlags.READABLE),
     }
 
     def __init__(self, orientation):
@@ -239,9 +239,9 @@ class HTray(Gtk.EventBox):
 
     __gproperties__ = {
         'align': (int, None, None, 0, 1, ALIGN_TO_START,
-                  GObject.PARAM_READWRITE |
-                  GObject.PARAM_CONSTRUCT_ONLY),
-        'drag-active': (bool, None, None, False, GObject.PARAM_READWRITE),
+                  GObject.ParamFlags.READWRITE |
+                  GObject.ParamFlags.CONSTRUCT_ONLY),
+        'drag-active': (bool, None, None, False, GObject.ParamFlags.READWRITE),
     }
 
     def __init__(self, **kwargs):
@@ -333,8 +333,8 @@ class VTray(Gtk.EventBox):
 
     __gproperties__ = {
         'align': (int, None, None, 0, 1, ALIGN_TO_START,
-                  GObject.PARAM_READWRITE | GObject.PARAM_CONSTRUCT_ONLY),
-        'drag-active': (bool, None, None, False, GObject.PARAM_READWRITE),
+                  GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY),
+        'drag-active': (bool, None, None, False, GObject.ParamFlags.READWRITE),
     }
 
     def __init__(self, **kwargs):
@@ -495,7 +495,7 @@ class TrayIcon(Gtk.ToolItem):
     def set_palette(self, palette):
         self._palette_invoker.palette = palette
 
-    palette = GObject.property(
+    palette = GObject.Property(
         type=object, setter=set_palette, getter=get_palette)
 
     def get_palette_invoker(self):
@@ -505,7 +505,7 @@ class TrayIcon(Gtk.ToolItem):
         self._palette_invoker.detach()
         self._palette_invoker = palette_invoker
 
-    palette_invoker = GObject.property(
+    palette_invoker = GObject.Property(
         type=object, setter=set_palette_invoker, getter=get_palette_invoker)
 
     def get_icon(self):

examples/progressicon.py
@@ -1,5 +1,5 @@
 from gi.repository import Gtk
-from gi.repository import GObject
+from gi.repository import GLib
 
 from sugar3.graphics.toolbarbox import ToolbarBox
 from sugar3.graphics.progressicon import ProgressIcon
@@ -51,7 +51,7 @@ def timeout_cb():
     return True
 
 
-GObject.timeout_add(50, timeout_cb)
+GLib.timeout_add(50, timeout_cb)
 
 
 if __name__ == '__main__':

src/sugar3/activity/activity.py
@@ -582,7 +582,7 @@ class Activity(Window, Gtk.Container):
             if not self._active and self._jobject:
                 self.save()
 
-    active = GObject.property(
+    active = GObject.Property(
         type=bool, default=False, getter=get_active, setter=set_active)
     '''
         Whether an activity is active.
@@ -621,7 +621,7 @@ class Activity(Window, Gtk.Container):
         '''
         self._max_participants = participants
 
-    max_participants = GObject.property(
+    max_participants = GObject.Property(
         type=int, default=0, getter=get_max_participants,
         setter=set_max_participants)
 

src/sugar3/graphics/alert.py
@@ -90,9 +90,9 @@ class Alert(Gtk.EventBox):
     }
 
     __gproperties__ = {
-        'title': (str, None, None, None, GObject.PARAM_READWRITE),
-        'msg': (str, None, None, None, GObject.PARAM_READWRITE),
-        'icon': (object, None, None, GObject.PARAM_WRITABLE),
+        'title': (str, None, None, None, GObject.ParamFlags.READWRITE),
+        'msg': (str, None, None, None, GObject.ParamFlags.READWRITE),
+        'icon': (object, None, None, GObject.ParamFlags.WRITABLE),
     }
 
     def __init__(self, **kwargs):

src/sugar3/graphics/animator.py
@@ -151,7 +151,7 @@ class Animator(GObject.GObject):
 
         if self._timeout_sid and \
            not hasattr(self._widget, 'add_tick_callback'):
-            GObject.source_remove(self._timeout_sid)
+            GLib.source_remove(self._timeout_sid)
             self._timeout_sid = 0
             self.emit('completed')
         if self._timeout_sid and hasattr(self._widget, 'add_tick_callback'):

src/sugar3/graphics/colorbutton.py
@@ -138,7 +138,7 @@ class _ColorButton(Gtk.Button):
     def get_color(self):
         return self._color
 
-    color = GObject.property(type=object, getter=get_color, setter=set_color)
+    color = GObject.Property(type=object, getter=get_color, setter=set_color)
 
     def set_icon_name(self, icon_name):
         '''
@@ -161,7 +161,7 @@ class _ColorButton(Gtk.Button):
         '''
         return self._preview.props.icon_name
 
-    icon_name = GObject.property(type=str,
+    icon_name = GObject.Property(type=str,
                                  getter=get_icon_name, setter=set_icon_name)
 
     def set_icon_size(self, icon_size):
@@ -170,7 +170,7 @@ class _ColorButton(Gtk.Button):
     def get_icon_size(self):
         return self._preview.props.icon_size
 
-    icon_size = GObject.property(type=int,
+    icon_size = GObject.Property(type=int,
                                  getter=get_icon_size, setter=set_icon_size)
 
     def set_title(self, title):
@@ -181,7 +181,7 @@ class _ColorButton(Gtk.Button):
     def get_title(self):
         return self._title
 
-    title = GObject.property(type=str, getter=get_title, setter=set_title)
+    title = GObject.Property(type=str, getter=get_title, setter=set_title)
 
     def _set_has_invoker(self, has_invoker):
         self._has_invoker = has_invoker
@@ -189,9 +189,9 @@ class _ColorButton(Gtk.Button):
     def _get_has_invoker(self):
         return self._has_invoker
 
-    has_invoker = GObject.property(type=bool, default=True,
-                                   flags=GObject.PARAM_READWRITE |
-                                   GObject.PARAM_CONSTRUCT_ONLY,
+    has_invoker = GObject.Property(type=bool, default=True,
+                                   flags=GObject.ParamFlags.READWRITE |
+                                   GObject.ParamFlags.CONSTRUCT_ONLY,
                                    getter=_get_has_invoker,
                                    setter=_set_has_invoker)
 
@@ -201,9 +201,9 @@ class _ColorButton(Gtk.Button):
     def _get_has_palette(self):
         return self._has_palette
 
-    has_palette = GObject.property(type=bool, default=True,
-                                   flags=GObject.PARAM_READWRITE |
-                                   GObject.PARAM_CONSTRUCT_ONLY,
+    has_palette = GObject.Property(type=bool, default=True,
+                                   flags=GObject.ParamFlags.READWRITE |
+                                   GObject.ParamFlags.CONSTRUCT_ONLY,
                                    getter=_get_has_palette,
                                    setter=_set_has_palette)
 
@@ -213,9 +213,9 @@ class _ColorButton(Gtk.Button):
     def _get_accept_drag(self):
         return self._accept_drag
 
-    accept_drag = GObject.property(type=bool, default=True,
-                                   flags=GObject.PARAM_READWRITE |
-                                   GObject.PARAM_CONSTRUCT_ONLY,
+    accept_drag = GObject.Property(type=bool, default=True,
+                                   flags=GObject.ParamFlags.READWRITE |
+                                   GObject.ParamFlags.CONSTRUCT_ONLY,
                                    getter=_get_accept_drag,
                                    setter=_set_accept_drag)
 
@@ -412,7 +412,7 @@ class _ColorPalette(Palette):
     def get_color(self):
         return self._color
 
-    color = GObject.property(type=object, getter=get_color, setter=set_color)
+    color = GObject.Property(type=object, getter=get_color, setter=set_color)
 
 
 def _add_accelerator(tool_button):
@@ -510,7 +510,7 @@ class ColorToolButton(Gtk.ToolItem):
         '''
         return self._accelerator
 
-    accelerator = GObject.property(type=str, setter=set_accelerator,
+    accelerator = GObject.Property(type=str, setter=set_accelerator,
                                    getter=get_accelerator)
 
     def create_palette(self):
@@ -546,7 +546,7 @@ class ColorToolButton(Gtk.ToolItem):
         self._palette_invoker.detach()
         self._palette_invoker = palette_invoker
 
-    palette_invoker = GObject.property(
+    palette_invoker = GObject.Property(
         type=object, setter=set_palette_invoker, getter=get_palette_invoker)
 
     def set_expanded(self, expanded):
@@ -584,7 +584,7 @@ class ColorToolButton(Gtk.ToolItem):
         '''
         return self.get_child().props.color
 
-    color = GObject.property(type=object, getter=get_color, setter=set_color)
+    color = GObject.Property(type=object, getter=get_color, setter=set_color)
 
     def set_icon_name(self, icon_name):
         '''
@@ -607,7 +607,7 @@ class ColorToolButton(Gtk.ToolItem):
         '''
         return self.get_child().props.icon_name
 
-    icon_name = GObject.property(type=str,
+    icon_name = GObject.Property(type=str,
                                  getter=get_icon_name, setter=set_icon_name)
 
     def set_icon_size(self, icon_size):
@@ -622,7 +622,7 @@ class ColorToolButton(Gtk.ToolItem):
         '''
         return self.get_child().props.icon_size
 
-    icon_size = GObject.property(type=int,
+    icon_size = GObject.Property(type=int,
                                  getter=get_icon_size, setter=set_icon_size)
 
     def set_title(self, title):
@@ -639,7 +639,7 @@ class ColorToolButton(Gtk.ToolItem):
         '''
         return self.get_child().props.title
 
-    title = GObject.property(type=str, getter=get_title, setter=set_title)
+    title = GObject.Property(type=str, getter=get_title, setter=set_title)
 
     def do_draw(self, cr):
         if self._palette and self._palette.is_up():

src/sugar3/graphics/combobox.py
@@ -65,7 +65,7 @@ class ComboBox(Gtk.ComboBox):
             return None
         return row[0]
 
-    value = GObject.property(
+    value = GObject.Property(
         type=object, getter=get_value, setter=None)
 
     def _get_real_name_from_theme(self, name, size):

commit 29e4452bbcb019cc77b55ed31a60871082d54afe

Review: Port from GObject to GLib
Fix new flake8 issues introduced by cb0d6e2.

src/sugar3/activity/activityfactory.py
@@ -236,8 +236,7 @@ class ActivityCreationHandler(GObject.GObject):
 
         GLib.child_watch_add(child.pid,
                              _child_watch_cb,
-                             (log_file,
-                             self._handle.activity_id))
+                             (log_file, self._handle.activity_id))
 
     def _no_reply_handler(self, *args):
         pass

src/sugar3/graphics/tray.py
@@ -37,8 +37,10 @@ class _TrayViewport(Gtk.Viewport):
 
     __gproperties__ = {
         'scrollable': (bool, None, None, False, GObject.ParamFlags.READABLE),
-        'can-scroll-prev': (bool, None, None, False, GObject.ParamFlags.READABLE),
-        'can-scroll-next': (bool, None, None, False, GObject.ParamFlags.READABLE),
+        'can-scroll-prev': (bool, None, None, False,
+                            GObject.ParamFlags.READABLE),
+        'can-scroll-next': (bool, None, None, False,
+                            GObject.ParamFlags.READABLE),
     }
 
     def __init__(self, orientation):
@@ -333,7 +335,8 @@ class VTray(Gtk.EventBox):
 
     __gproperties__ = {
         'align': (int, None, None, 0, 1, ALIGN_TO_START,
-                  GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY),
+                  GObject.ParamFlags.READWRITE |
+                  GObject.ParamFlags.CONSTRUCT_ONLY),
         'drag-active': (bool, None, None, False, GObject.ParamFlags.READWRITE),
     }
 