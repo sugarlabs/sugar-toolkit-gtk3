sugarlabs/sugar-toolkit-gtk3

commit 3b28597486951c225239c220def190377ee1739f

flake8 fixes

src/sugar3/activity/activityinstance.py
@@ -135,7 +135,8 @@ def main():
         command = bundle.get_command()
         if command.startswith('sugar-activity'):
             if not command.startswith('sugar-activity3'):
-                logging.warning("Activity written for Python 2, consider porting to Python 3.")
+                logging.warning("Activity written for Python 2,\
+                                consider porting to Python 3.")
             activity_class = command.split(" ")[1]
 
     # when an activity is started outside sugar,

src/sugar3/activity/widgets.py
@@ -34,7 +34,8 @@ from sugar3.graphics.palettemenu import PaletteMenuBox
 from sugar3 import profile
 
 
-def _(msg): return gettext.dgettext('sugar-toolkit-gtk3', msg)
+def _(msg):
+    return gettext.dgettext('sugar-toolkit-gtk3', msg)
 
 
 def _create_activity_icon(metadata):
@@ -337,7 +338,8 @@ class ActivityToolbar(Gtk.Toolbar):
 
         if activity.metadata:
             title_button = TitleEntry(activity)
-            title_button.connect('enter-key-press', lambda widget: self.emit('enter-key-press'))
+            title_button.connect('enter-key-press',
+                                 lambda widget: self.emit('enter-key-press'))
             title_button.show()
             self.insert(title_button, -1)
             self.title = title_button.entry

src/sugar3/bundle/__init__.py
@@ -29,10 +29,12 @@ an `[Activity]` header on the first line:
   1.2.3, 1.2.3-country, or 1.2.3~developer,
 
 * `bundle_id` - the activity bundle identifier, using [Java package
-  naming conventions](http://en.wikipedia.org/wiki/Java_package#Package_naming_conventions),
+  naming conventions]
+  (http://en.wikipedia.org/wiki/Java_package#Package_naming_conventions),
   should conform to the [D-Bus specification for message protocol
-  names](http://dbus.freedesktop.org/doc/dbus-specification.html#message-protocol-names)(hyphens
-  are not allowed), usually an organisation or individual domain name
+  names]
+  (http://dbus.freedesktop.org/doc/dbus-specification.html#message-protocol-names)
+  (hyphens are not allowed), usually an organisation or individual domain name
   in reverse order, e.g. `org.sugarlabs.Name`,
 
 * `license` - an identifier for the software license of the bundle,
@@ -58,7 +60,8 @@ Optional metadata keys are;
   files of these MIME types.  Used to offer your activity when opening a
   downloaded file or a journal object.
 
-* `url` - link to a home page or user documentation on https://help.sugarlabs.org/,
+* `url` - link to a home page or user documentation on
+  https://help.sugarlabs.org/,
 
 * `repository` - link to repository for activity code, for use by git clone,
 
@@ -84,7 +87,8 @@ Deprecated metadata keys are;
 * `update_url` - the updater no longer uses this.
 
 .. _SPDX License Identifier: http://spdx.org/licenses/
-.. _Fedora License Short Name: https://fedoraproject.org/wiki/Licensing:Main?rd=Licensing#Good_Licenses
+.. _Fedora License Short Name:
+  https://fedoraproject.org/wiki/Licensing:Main?rd=Licensing#Good_Licenses
 
 AppStream Metadata
 ==================
@@ -132,12 +136,27 @@ Example `activity.info`
 
     metadata_license = CC0-1.0
     description:
-        <p>Surf the world! Here you can do research, watch educational videos, take online courses, find books, connect with friends and more.  Browse is powered by the WebKit2 rendering engine with the Faster Than Light javascript interpreter - allowing you to view the full beauty of the web.</p>
+        <p>
+        Surf the world! Here you can do research, watch educational videos,
+        take online courses, find books, connect with friends and more.
+        Browse is powered by the WebKit2 rendering engine with the Faster
+        Than Light javascript interpreter - allowing you to view the
+        full beauty of the web.
+        </p>
         <p>To help in researching, Browse offers many features:</p>
         <ul>
-            <li>Bookmark (save) good pages you find - never loose good resources or forget to add them to your bibliography</li>
-            <li>Bookmark pages with collaborators in real time - great for researching as a group or teachers showing pages to their class</li>
-            <li>Comment on your bookmarked pages - a great tool for making curated collections</li>
+            <li>
+            Bookmark (save) good pages you find - never loose good resources
+            or forget to add them to your bibliography
+            </li>
+            <li>
+            Bookmark pages with collaborators in real time - great for
+            researching as a group or teachers showing pages to their class
+            </li>
+            <li>
+            Comment on your bookmarked pages - a great tool for making curated
+            collections
+            </li>
         </ul>
     screenshots = https://people.sugarlabs.org/sam/activity-ss/browse-1-1.png https://people.sugarlabs.org/sam/activity-ss/browse-1-2.png
 '''

src/sugar3/bundle/bundleversion.py
@@ -43,7 +43,8 @@ Based on the implementation of :pep:`386`, but adapted to our
 numeration schema.
 
 Attributes:
-    VERSION_RE (RegexObject): regular expression for versions, deprecated, as it is insufficient by itself.
+    VERSION_RE (RegexObject): regular expression for versions,\
+    deprecated, as it is insufficient by itself.
 """
 
 import re

src/sugar3/dispatch/saferef.py
@@ -167,7 +167,8 @@ class BoundMethodWeakref(object):
     def __cmp__(self, other):
         """Compare with another reference"""
         if not isinstance(other, self.__class__):
-            return ((self.__class__ > type(other)) - (self.__class__ < type(other)))
+            return ((self.__class__ > type(other)) -
+                    (self.__class__ < type(other)))
         return ((self.key > other.key) - (self.key < other.key))
 
     def __call__(self):

src/sugar3/graphics/alert.py
@@ -61,7 +61,8 @@ from sugar3.graphics import style
 from sugar3.graphics.icon import Icon
 
 
-def _(msg): return gettext.dgettext('sugar-toolkit-gtk3', msg)
+def _(msg):
+    return gettext.dgettext('sugar-toolkit-gtk3', msg)
 
 
 if not hasattr(GObject.ParamFlags, 'READWRITE'):

src/sugar3/graphics/colorbutton.py
@@ -29,7 +29,8 @@ from sugar3.graphics.icon import Icon
 from sugar3.graphics.palette import Palette, ToolInvoker, WidgetInvoker
 
 
-def _(msg): return gettext.dgettext('sugar-toolkit-gtk3', msg)
+def _(msg):
+    return gettext.dgettext('sugar-toolkit-gtk3', msg)
 
 
 if not hasattr(GObject.ParamFlags, 'READWRITE'):
@@ -123,7 +124,8 @@ class _ColorButton(Gtk.Button):
         context = self.get_style_context()
         fg_color = context.get_color(Gtk.StateType.NORMAL)
         # the color components are stored as float values between 0.0 and 1.0
-        return '#%.2X%.2X%.2X' % (int(fg_color.red * 255), int(fg_color.green * 255),
+        return '#%.2X%.2X%.2X' % (int(fg_color.red * 255),
+                                  int(fg_color.green * 255),
                                   int(fg_color.blue * 255))
 
     def set_color(self, color):

src/sugar3/graphics/icon.py
@@ -209,7 +209,7 @@ class _IconBuffer(object):
             # try read from the .icon file
             icon_filename = info.get_filename().replace('.svg', '.icon')
             if icon_filename != info.get_filename() and \
-                            os.path.exists(icon_filename):
+                    os.path.exists(icon_filename):
 
                 try:
                     with open(icon_filename) as config_file:
@@ -1309,8 +1309,7 @@ class CellRendererIcon(Gtk.CellRenderer):
                 # widget is the treeview
                 x, y = widget.get_pointer()
                 x, y = widget.convert_widget_to_bin_window_coords(x, y)
-                return ((cell_area.x <= x <= cell_area.x + cell_area.width)
-                        and
+                return ((cell_area.x <= x <= cell_area.x + cell_area.width) and
                         (cell_area.y <= y <= cell_area.y + cell_area.height))
 
             pointer_inside = is_pointer_inside()
@@ -1379,10 +1378,10 @@ def get_icon_state(base_name, perc, step=5):
     '''
     Get the closest icon name for a given state in percent.
 
-    First, you need a set of icons.  They must be prefixed with `base_name`, for
-    example "network-wireless".  They must be suffixed with 3 digit percentage
-    numbers, for example "-000", "-200", etc.  Eventually, you get a collection
-    of icon names like:
+    First, you need a set of icons. They must be prefixed with `base_name`,
+    for example "network-wireless". They must be suffixed with 3 digit
+    percentage numbers, for example "-000", "-200", etc. Eventually, you get
+    a collection of icon names like:
 
     * network-wireless-000
     * network-wireless-020

commit 90c2539135168b8e9721d3e663609c62b9826cea

Fix unnecessary whitespace in warning
Regression introduced by 3b28597 ("flake8 fixes").

Fixes https://github.com/sugarlabs/sugar-toolkit-gtk3/issues/424

src/sugar3/activity/activityinstance.py
@@ -135,8 +135,8 @@ def main():
         command = bundle.get_command()
         if command.startswith('sugar-activity'):
             if not command.startswith('sugar-activity3'):
-                logging.warning("Activity written for Python 2,\
-                                consider porting to Python 3.")
+                logging.warning("Activity written for Python 2,"
+                                " consider porting to Python 3.")
             activity_class = command.split(" ")[1]
 
     # when an activity is started outside sugar,