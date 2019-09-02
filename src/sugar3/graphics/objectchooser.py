sugarlabs/sugar-toolkit-gtk3

commit aa8a5e70c415e6c2acf4ff373d9b366ac4692bb1

Port from Python 2 to six
Signed-off-by: James Cameron <quozl@laptop.org>

src/sugar3/graphics/objectchooser.py
@@ -19,8 +19,8 @@
 STABLE.
 """
 
+import six
 import logging
-import StringIO
 import cairo
 
 from gi.repository import GObject
@@ -59,13 +59,13 @@ def get_preview_pixbuf(preview_data, width=-1, height=-1):
         None, if it could not be created
 
     Example:
-        pixbuf = get_preview_pixbuf(metadata.get('preview', ''))                
-        has_preview = pixbuf is not None                                        
-                                                                               
-        if has_preview:                                                        
-            im = Gtk.Image()                                                    
-            im.set_from_pixbuf(pixbuf)                                          
-            box.add(im)                                                        
+        pixbuf = get_preview_pixbuf(metadata.get('preview', ''))
+        has_preview = pixbuf is not None
+
+        if has_preview:
+            im = Gtk.Image()
+            im.set_from_pixbuf(pixbuf)
+            box.add(im)
             im.show()
     """
     if width == -1:
@@ -82,7 +82,7 @@ def get_preview_pixbuf(preview_data, width=-1, height=-1):
             import base64
             preview_data = base64.b64decode(preview_data)
 
-        png_file = StringIO.StringIO(preview_data)
+        png_file = six.StringIO(preview_data)
         try:
             # Load image and scale to dimensions
             surface = cairo.ImageSurface.create_from_png(png_file)
@@ -126,7 +126,7 @@ class ObjectChooser(object):
         what_filter (str): an activity bundle_id or a generic mime type as
             defined in :mod:`sugar3.mime` used to determine which objects
             will be presented in the object chooser
-        
+
         filter_type (str): should be one of [None, FILTER_TYPE_GENERIC_MIME,
             FILTER_TYPE_ACTIVITY, FILTER_TYPE_MIME_BY_ACTIVITY]
 
@@ -155,7 +155,7 @@ class ObjectChooser(object):
 
     Examples:
         chooser = ObjectChooser(self._activity, what_filter='Image')
-        
+
         chooser = ObjectChooser(parent=self,
                                 what_filter=self.get_bundle_id(),
                                 filter_type=FILTER_TYPE_ACTIVITY)
@@ -192,7 +192,7 @@ class ObjectChooser(object):
     def run(self):
         """
         Runs the object chooser and displays it.
-        
+
         Returns:
             Gtk.ResponseType constant, the response received from displaying the
                 object chooser.

commit 6a9ccb5e808f4a63f415acfbc10dcde8e97fc948

Port to Python 3 - preview metadata is bytes
Preview metadata in 0.114 and earlier was returned by datastore client
as a dbus.ByteArray.

Regression introduced by aa8a5e7 ("Port from Python 2 to six").

Traceback (most recent call last):
  File "/usr/lib/python3.7/dist-packages/jarabe/journal/expandedentry.py", line 378, in _create_preview
    pixbuf = get_preview_pixbuf(metadata.get('preview', ''))
  File "/usr/lib/python3.7/dist-packages/sugar3/graphics/objectchooser.py", line 85, in get_preview_pixbuf
    preview_data = base64.b64decode(preview_data)
  File "/usr/lib/python3.7/base64.py", line 87, in b64decode
    return binascii.a2b_base64(s)
binascii.Error: Invalid base64-encoded string: number of data characters (237) cannot be 1 more than a multiple of 4

Related to
https://github.com/sugarlabs/sugar-datastore/issues/16

src/sugar3/graphics/objectchooser.py
@@ -79,12 +79,12 @@ def get_preview_pixbuf(preview_data, width=-1, height=-1):
     pixbuf = None
 
     if len(preview_data) > 4:
-        if preview_data[1:4] != 'PNG':
+        if preview_data[1:4] != b'PNG':
             # TODO: We are close to be able to drop this.
             import base64
             preview_data = base64.b64decode(preview_data)
 
-        png_file = six.StringIO(preview_data)
+        png_file = six.BytesIO(preview_data)
         try:
             # Load image and scale to dimensions
             surface = cairo.ImageSurface.create_from_png(png_file)