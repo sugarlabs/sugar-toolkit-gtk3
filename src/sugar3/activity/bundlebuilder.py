9605f81824e61cfb4e9e0f675450dc452a50966c
Ibiam Chihurumnaya <ibiamchihurumnaya@gmail.com>
2021-07-29 17:39:45 +0100

    PermissionError when trying to install an activity
    Python currently has a bug that makes it difficult
    to use os.makedirs as it can't recursively apply
    modes as of Python 3.7, this commit aims to use the shell
    to create the directories, this commit aims to use the shell
    to create the directories.
    
    Signed-off-by: Ibiam Chihurumnaya <ibiamchihurumnaya@gmail.com>

--- src/sugar3/activity/bundlebuilder.py
+++ src/sugar3/activity/bundlebuilder.py
@@ -316,7 +316,9 @@ class Installer(Packager):
 
             path = os.path.dirname(dest)
             if not os.path.exists(path):
-                os.makedirs(path)
+                # os.makedirs(path)
+                args = ['mkdir', '-p', path]
+                subprocess.run(args)
 
             shutil.copy(source, dest)
 
