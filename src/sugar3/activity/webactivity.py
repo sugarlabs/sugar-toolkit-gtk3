0bd9caf0b791b2c67ab25d4589dfce49f9797f96
Chihurumnaya Ibiam <ibiam@sugarlabs.org>
2024-02-01 11:00:46 -0500

    Port to WebKit2 4.1
    WebKit24.1 has no API changes, but it requires libsoup 3.0 which
    the toolkit doesn't use.
    
    Run autoupdate
    
    Signed-off-by: Chihurumnaya Ibiam <ibiam@sugarlabs.org>

--- src/sugar3/activity/webactivity.py
+++ src/sugar3/activity/webactivity.py
@@ -22,7 +22,7 @@ import logging
 import gi
 gi.require_version('Gdk', '3.0')
 gi.require_version('Gtk', '3.0')
-gi.require_version('WebKit2', '4.0')
+gi.require_version('WebKit2', '4.1')
 
 from gi.repository import Gdk
 from gi.repository import Gio
